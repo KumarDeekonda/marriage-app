e7be4fb7e2657612b4332f52f829916ee6789650
KumarDeekonda <kumardvplr@gmail.com>
2018-08-11 01:58:48 -0400

    removed dependencies in module for countdowntimers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NgxGalleryModule } from 'ngx-gallery';
-import { CountdownTimerModule } from 'ngx-countdown-timer';
-import { CountdownModule } from 'ng2-countdown-timer';
+
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NgxGalleryModule, CountdownTimerModule.forRoot()],
+  imports: [BrowserModule, NgxGalleryModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
